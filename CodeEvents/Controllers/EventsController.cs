c1a7b19a925d464a1182eec26485d31cc2077439
emilyalbert <FunkCrusade@outlook.com>
2023-12-04 23:53:58 -0600

    codeevents changed before connecting a database

---
+++ CodeEvents/Controllers/EventsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeEvents.Data;
+using CodeEvents.Models;
+using Microsoft.AspNetCore.Mvc;
+using CodeEvents.ViewModel;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CodeEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List <Event> events = new List<Event>(EventData.GetAll());
+
+            return View(events);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            AddEventViewModel addEventViewModel = new AddEventViewModel();
+            return View(addEventViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddEventViewModel addEventViewModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Event newEvent = new Event
+                {
+                    Name = addEventViewModel.Name,
+                    Description = addEventViewModel.Description,
+                    ContactEmail = addEventViewModel.ContactEmail,
+                    EventLocation = addEventViewModel.EventLocation
+                };
+                EventData.Add(newEvent);
+
+                return Redirect("/Events");
+            }
+            return View(addEventViewModel);
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
+                return Redirect("/Events");
+        }
+    }
+}
+
---
+++ CodeEvents/ViewModel/AddEventViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeEvents.ViewModel
+{
+	public class AddEventViewModel
+	{
+		[Required(ErrorMessage = "Name of event is required. ")]
+		[StringLength(50, MinimumLength =3, ErrorMessage ="Event name should be between 3-50 characters. ")]
+		public string? Name { get; set; }
+
+		[Required(ErrorMessage = "Event description is required. ")]
+		[StringLength(500, ErrorMessage ="Description is over 500 characters")]
+		public string? Description { get; set; }
+
+		[Required(ErrorMessage ="Event needs a location")]
+		[StringLength(500, MinimumLength = 5, ErrorMessage ="location must be at least 5 letters long. ")]
+		public string? EventLocation { get; set; }
+
+		[EmailAddress]
+		public string? ContactEmail { get; set; }
+	}
+}
+
